f4e24e642f33957790742e900ec907ea1f244c5c
Igor <igor.goryainov@altarix.ru>
2016-03-25 09:42:45 +0400

    Optimized imports.

--- app/src/main/java/testmap/com/testmap/fragment/MapFragment.java
+++ app/src/main/java/testmap/com/testmap/fragment/MapFragment.java
@@ -1,11 +1,6 @@
 package testmap.com.testmap.fragment;
 
-import android.location.Location;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
